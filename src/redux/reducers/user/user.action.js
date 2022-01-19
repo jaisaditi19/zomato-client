29e4a2cf7f4fa6d455553a92f55d77e16cf31327
jaisaditi19 <85386907+jaisaditi19@users.noreply.github.com>
2022-01-19 13:19:47 +0530

    Update user.action.js

--- src/redux/reducers/user/user.action.js
+++ src/redux/reducers/user/user.action.js
@@ -7,7 +7,7 @@ export const getUser = (_id) => async (dispatch) => {
   try {
     const User = await axios({
       method: "GET",
-      url: `http://localhost:4000/user/${_id}`,
+      url: `https://zomato-server1.herokuapp.com/user/${_id}`,
     })
 
     return dispatch({ type: GET_USER, payload: User.data });
@@ -20,7 +20,7 @@ export const getMySelf = () => async (dispatch) => {
   try {
     const User = await axios({
       method: "GET",
-      url: `http://localhost:4000/user/`,
+      url: `https://zomato-server1.herokuapp.com/user/`,
     });
 
     return dispatch({ type: SELF, payload: User.data });
