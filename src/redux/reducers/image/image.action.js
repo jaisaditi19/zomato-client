f6179b5463f81fa6bed8b76348d6250d2b7f2866
jaisaditi19 <85386907+jaisaditi19@users.noreply.github.com>
2022-01-19 13:18:07 +0530

    Update image.action.js

--- src/redux/reducers/image/image.action.js
+++ src/redux/reducers/image/image.action.js
@@ -7,7 +7,7 @@ export const getImage = (_id) => async (dispatch) => {
     try {
         const image = await axios ({
             method: "GET",
-            url: `http://localhost:4000/image/${_id}`,
+            url: `https://zomato-server1.herokuapp.com/image/${_id}`,
         });
 
         return dispatch({type: GET_IMAGE, payload: image.data});
